d39ccc265214b91b5f3a92e2851a645b6fda9497
Alexander Hermes <alexander.hermes@accellion.com>
2018-05-23 11:36:06 +0800

    Add project files.

---
+++ zabbix-o365-hook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zabbix_o365_hook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
